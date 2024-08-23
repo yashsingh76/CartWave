4f70e69fd6901967624065e35b1b721404206e9a
Yash Pratap Singh <yashsingh7606@gmail.com>
2024-08-23 17:30:52 +0530

    project-setup

---
+++ src/main/java/com/tier3hub/CartWave/CartWaveApplication.java
@@ -0,0 +1,13 @@
+package com.tier3hub.CartWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tier3hub/CartWave/CartWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tier3hub.CartWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
